775f80ecbda4c554d8e7c3cf15391455aad6b7be
OscarValcarce <oscarix29@gmail.com>
2023-04-28 09:58:10 +0200

    Estructuro todos los ficheros

--- funciones.h
+++ funciones.h
@@ -5,4 +5,32 @@
 #ifndef PRACTICA6_FUNCIONES_H
 #define PRACTICA6_FUNCIONES_H
 
+#define MAX_PERSONAS 100
+#define L 100
+enum contacto{
+    Favorito,
+    Conocido,
+    Trabajo
+};
+
+typedef struct Persona {
+    char nombre[MAX_PERSONAS];
+    char apellidos[MAX_PERSONAS];
+    char numero[MAX_PERSONAS];
+    short int edad;
+    enum contacto c;
+} tPersona;
+
+typedef struct Agenda {
+    tPersona *personas;
+    int num_personas;
+} tAgenda;
+
+void inicializar_agenda(tAgenda *agenda);
+void mostrar_personas(tAgenda *agenda);
+void nueva_persona(tAgenda *agenda);
+void borrar_persona(tAgenda *agenda);
+void crear_fichero(tAgenda * agenda);
+void leer_fichero(tAgenda * agenda);
+
 #endif //PRACTICA6_FUNCIONES_H
